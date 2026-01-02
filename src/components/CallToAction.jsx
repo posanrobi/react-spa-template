ffcf821abacef3d75e289ac1b560b358b8739ed9
Pósán Róbert <posanrobi@gmail.com>
2026-01-03 00:13:50 +0100

    test figyelmeztetés

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -2,7 +2,8 @@ const CallToAction = () => {
     return (
         <section className="cta">
             <h2>Készen állsz az indulásra?</h2>
-            <p>Vedd fel velünk a kapcsolatot és kezdjük el még ma.</p>
+            <p>NE adj meg személyes adatokat, mert egy teszt oldal!</p>
+            {/* <p>Vedd fel velünk a kapcsolatot és kezdjük el még ma.</p> */}
             <button className="cta__button">Kapcsolat</button>
         </section>
     );
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,8 @@ const Contact = () => {
     return (
         <section id="contact" className="contact">
             <h2>Készen állsz az indulásra?</h2>
-            <p>Vedd fel velünk a kapcsolatot és kezdjük el még ma.</p>
+            <p>NE adj meg személyes adatokat, mert egy teszt oldal!</p>
+            {/*  <p>Vedd fel velünk a kapcsolatot és kezdjük el még ma.</p> */}
             <GoogleReCaptchaProvider reCaptchaKey="6LdIYTosAAAAAPOqDB66vJ9WnPLy_a_lt8YgCuzm">
                 {/*  <ContactForm /> */}
             </GoogleReCaptchaProvider>
